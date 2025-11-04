6ee15c6612f92e76755f785f83ebe987a7f6ce6a
BPOTW <133132057+BPOTW@users.noreply.github.com>
2025-11-04 11:08:59 +0500

    Update redirection.jsx

--- src/redirection.jsx
+++ src/redirection.jsx
@@ -8,7 +8,7 @@ const Redirection = () => {
   useEffect(() => {
     const getLinkData = async () => {
       try {
-        const res = await axios.get(`https://database-production-1030.up.railway.app:5000/${id}`);
+        const res = await axios.get(`https://database-production-1030.up.railway.app/${id}`);
         const redirectUrl = await res.data[0].originalLink;
         console.log(redirectUrl);
 
