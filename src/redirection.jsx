596119c2298d7e027f546fdf9113da2b30863f52
BPOTW <133132057+BPOTW@users.noreply.github.com>
2025-11-11 21:22:31 +0500

    Update redirection.jsx

--- src/redirection.jsx
+++ src/redirection.jsx
@@ -13,7 +13,7 @@ const Redirection = () => {
         const redirectUrl = res.data.originalLink;
         console.log(redirectUrl);
 
-        window.open(`https://${redirectUrl}`, "_self");
+        window.open(`${redirectUrl}`, "_self");
       } catch (err) {
         console.error("Error fetching:", err);
         // navigate("/error");
